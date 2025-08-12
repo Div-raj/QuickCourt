fef7c2a42b0494d54cedf77690533a926cf36ce2
Aman Verma <126273748+Xavermax@users.noreply.github.com>
2025-08-12 10:19:01 +0530

    Delete config.ts

--- config.ts
+++
@@ -1,3 +0,0 @@
-// In a real-world application, this key should be stored in an environment variable
-// and not be hardcoded in the source code for security reasons.
-export const GEOAPIFY_API_KEY = "ff7dcac22fc64d9d8a92c454916d9226";
